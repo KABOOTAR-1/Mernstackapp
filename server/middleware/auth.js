50d77dfb2777b3b34d784c01e5e71c1655803b51
KABOOTAR-1 <bt20cse052@iiitn.ac.in>
2023-08-08 08:52:15 +0530

    Remove

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -11,9 +11,12 @@ export const verifyToken = async (req, res, next) => {
     if (token.startsWith("Bearer ")) {
       token = token.slice(7, token.length).trimLeft();
     }
-
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = verified;
+    try {
+      const verified = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = verified;
+    } catch (err) {
+      console.log(err.message);
+    }
     next();
   } catch (err) {
     res.status(500).json({ error: err.message });
